464c39076d56548994037a51ee0c7e73dba1de66
Diogo Duarte <diogord@al.insper.edu.br>
2023-11-05 13:15:18 -0300

    Botoes direita e esquerda funcionando

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     public float jumpForce = 5f; // Adjust the jump force in the Inspector
+
+    public float Speed = 5f;
     private bool isJumping = false;
     private Rigidbody2D rb;
 
@@ -29,5 +31,15 @@ public class InputManager : MonoBehaviour
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         isJumping = true;
         }
+    }
+
+    public void OnRightHold()
+    {          
+        rb.velocity = new Vector2(Speed,0);
+    }
+
+    public void OnLeftHold()
+    {
+         rb.velocity = new Vector2(-Speed,0);
     }
 }
